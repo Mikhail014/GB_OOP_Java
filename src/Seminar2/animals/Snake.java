e5f8df294d7c40efc118b1c02bd9f82e3f93f412
Mikhail1417 <seminma@clubpixel.ru>
2023-02-19 01:13:43 +0300

    Класс Snake

---
+++ src/Seminar2/animals/Snake.java
@@ -0,0 +1,36 @@
+package Seminar2.animals;
+
+public class Snake extends Animal{
+    private int bodyLength;
+    protected static final int MAX_WEIGHT = 100;
+    protected static final String TYPE = "Snake";
+
+    public Snake() {}
+
+    public Snake(int bodyLength) {
+        this.bodyLength = bodyLength;
+    }
+
+    public Snake(int age, int weight, int numberOfLimbs, int bodyLength) {
+        super(age, weight, numberOfLimbs);
+        this.bodyLength = bodyLength;
+    }
+
+    public float getBodyLength() {
+        return bodyLength;
+    }
+
+    public void setBodyLength(int bodyLength) {
+        this.bodyLength = bodyLength;
+    }
+
+    @Override
+    public String getType() {
+        return "Змея";
+    }
+
+    @Override
+    public int getMaxWeight() {
+        return MAX_WEIGHT;
+    }
+}
