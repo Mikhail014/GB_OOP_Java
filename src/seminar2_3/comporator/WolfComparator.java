a35ccbef3b1708d5886862e94b4b200a64c2a163
Mikhail1417 <seminma@clubpixel.ru>
2023-02-21 09:06:07 +0300

    Рефакторинг метода compare в классе WolfComparator

--- src/seminar2_3/comporator/WolfComparator.java
+++ src/seminar2_3/comporator/WolfComparator.java
@@ -7,11 +7,9 @@ import java.util.Comparator;
 public class WolfComparator implements Comparator<Wolf> {
     @Override
     public int compare(Wolf o1, Wolf o2) {
-        if (o1.getWeight() > o2.getWeight() && o1.getAge() > o2.getAge()){
-            return 1;
-        } else if (o1.getWeight() < o2.getWeight() && o1.getAge() < o2.getAge()) {
-            return -1;
+        if (o1.getAge() != o2.getAge()){
+            return Integer.compare(o1.getAge(), o2.getAge());
         }
-        return 0;
+        return Integer.compare(o1.getWeight(), o2.getWeight());
     }
 }
